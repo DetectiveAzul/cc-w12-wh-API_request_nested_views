35e599e527caf9445ee9d4248b6b48e8172087c5
Marta Beveridge <marta@beveridge.uk>
2018-05-25 14:37:36 +0100

    Title and description of all movies are rendered on the website

--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -9,4 +9,18 @@ MovieView.prototype.createElement = function () {
   return this.element = movieDiv;
 };
 
+MovieView.prototype.render = function() {
+  this.renderHTMLElement('h2', 'movie-title', 'title');
+  this.renderHTMLElement('p', 'movie-description', 'description');
+};
+
+
+
+MovieView.prototype.renderHTMLElement = function(elementType, htmlClass, content) {
+  const element = document.createElement(elementType);
+  element.classList.add(htmlClass);
+  element.textContent = this.movie[content];
+  this.element.appendChild(element);
+}
+
 module.exports = MovieView;
